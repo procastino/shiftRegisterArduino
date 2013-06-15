f315ae76a8499869c308ed4f6da1430fe485ef1a
tucho <tucho@negrinho.(none)>
2013-06-15 23:13:42 +0200

    Engadido programa. Funciona con dous botóns para contar programa mais e menos.

---
+++ shiftregister/shiftregister.ino
@@ -0,0 +1,97 @@
+    /*
+    Adafruit Arduino - Lesson 4. 8 LEDs and a Shift Register
+    */
+     
+    #define pinBotonMais 3
+    #define pinBotonMenos 2
+    
+    int estadoBotonMais;
+    int estadoBotonMenos;
+    int ultimoEstadoBotonMais=LOW;
+    int ultimoEstadoBotonMenos=LOW;
+    
+    long ultimoDebounce=0;
+    long retardoDebounce=50;
+    
+    int latchPin = 5;
+    int clockPin = 6;
+    int dataPin = 4;
+    
+    int programa=0;
+    
+    byte leds = 0;
+     
+    void setup()
+    {
+    pinMode(pinBotonMais, INPUT);
+    pinMode(pinBotonMenos, INPUT);
+    pinMode(latchPin, OUTPUT);
+    pinMode(dataPin, OUTPUT);
+    pinMode(clockPin, OUTPUT);
+    
+    Serial.begin(9600);
+    }
+     
+    void loop()
+    {
+    int lecturaMais=digitalRead(pinBotonMais);
+
+    if (lecturaMais!=ultimoEstadoBotonMais) {
+      ultimoDebounce=millis();
+    }
+    
+    if ((millis()-ultimoDebounce)>retardoDebounce) {
+      if (lecturaMais!=estadoBotonMais) {
+        estadoBotonMais=lecturaMais;
+        
+        if (estadoBotonMais==HIGH) {
+          programa=programa+1;
+          Serial.println("programa");
+          Serial.println(programa);
+        }
+      }
+    }
+    
+    ultimoEstadoBotonMais=lecturaMais;
+    
+    
+    int lecturaMenos=digitalRead(pinBotonMenos);
+
+    if (lecturaMenos!=ultimoEstadoBotonMenos) {
+      ultimoDebounce=millis();
+    }
+    
+    if ((millis()-ultimoDebounce)>retardoDebounce) {
+      if (lecturaMenos!=estadoBotonMenos) {
+        estadoBotonMenos=lecturaMenos;
+        
+        if (estadoBotonMenos==HIGH) {
+          programa--;
+          Serial.println("programa");
+          Serial.println(programa);
+        }
+      }
+    }
+    
+    ultimoEstadoBotonMenos=lecturaMenos;
+    
+//    
+//    leds = 0;
+//    updateShiftRegister();
+//    delay(500);
+//    for (int i = 0; i < 8; i++)
+//    {
+//    bitSet(leds, i);
+//    updateShiftRegister();
+//    delay(500);
+//    }
+    }
+     
+//    void updateShiftRegister()
+//    {
+//    digitalWrite(latchPin, LOW);
+//    shiftOut(dataPin, clockPin, LSBFIRST, leds);
+//    digitalWrite(latchPin, HIGH);
+//    }
+//    
+//    
