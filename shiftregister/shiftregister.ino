943306074408a03f089d76286b26d1c1a01800b3
tucho <tucho@negrinho.(none)>
2013-06-16 14:54:22 +0200

    Erro de subida, me lio coas versions :) CORRIXIDO

--- shiftregister/shiftregister.ino
+++ shiftregister/shiftregister.ino
@@ -2,16 +2,20 @@
     Adafruit Arduino - Lesson 4. 8 LEDs and a Shift Register
     */
      
-    #define pinBotonMais 3
-    #define pinBotonMenos 2
+//    #define pinBotonMais 3
+//    #define pinBotonMenos 2
     
-    int estadoBotonMais;
-    int estadoBotonMenos;
-    int ultimoEstadoBotonMais=LOW;
-    int ultimoEstadoBotonMenos=LOW;
+    int pinBotons[]={3,2};
+    int ultimoEstadoBoton[]={LOW,LOW};
+    int estadoBoton[]={LOW,LOW};
+    
+//    int estadoBotonMais;
+//    int estadoBotonMenos;
+//    int ultimoEstadoBotonMais=LOW;
+//    int ultimoEstadoBotonMenos=LOW;
     
     long ultimoDebounce=0;
-    long retardoDebounce=50;
+    long retardoDebounce=20;
     
     int latchPin = 5;
     int clockPin = 6;
@@ -23,8 +27,10 @@
      
     void setup()
     {
-    pinMode(pinBotonMais, INPUT);
-    pinMode(pinBotonMenos, INPUT);
+    for (int i=0;i<2;i++)
+    {
+      pinMode(pinBotons[i],INPUT);
+    }
     pinMode(latchPin, OUTPUT);
     pinMode(dataPin, OUTPUT);
     pinMode(clockPin, OUTPUT);
@@ -34,54 +40,10 @@
      
 void loop()
 {
-    int lecturaMais=digitalRead(pinBotonMais);
-
-    if (lecturaMais!=ultimoEstadoBotonMais) {
-      ultimoDebounce=millis();
-      }
-    
-    if ((millis()-ultimoDebounce)>retardoDebounce) 
-    {
-      if (lecturaMais!=estadoBotonMais) 
-      {
-        estadoBotonMais=lecturaMais;
-        
-        if (estadoBotonMais==HIGH) 
-        {
-          programa=programa+1;
-          Serial.println("programa");
-          Serial.println(programa);
-          }
-        }
-      }
-    
-    ultimoEstadoBotonMais=lecturaMais;
-    
-    
-    int lecturaMenos=digitalRead(pinBotonMenos);
-
-    if (lecturaMenos!=ultimoEstadoBotonMenos) 
-    {
-      ultimoDebounce=millis();
-    }
-    
-    if ((millis()-ultimoDebounce)>retardoDebounce) 
-    {
-      if (lecturaMenos!=estadoBotonMenos) 
-      {
-        estadoBotonMenos=lecturaMenos;
-        
-        if (estadoBotonMenos==HIGH) 
-        {
-          programa--;
-          Serial.println("programa");
-          Serial.println(programa);
-        }
-      }
-    }
-    
-    ultimoEstadoBotonMenos=lecturaMenos;
-    
+  
+  lecturaBotons();
+ 
+   
     switch (programa) 
     {
        case 0:
@@ -89,48 +51,86 @@ void loop()
          break;
        
        case 1:
+         acesos();
+         break;
+       
+       case 2:
          encendidoProgresivo();
          break;
+       
+       case 3:
+         encendidoProgresivoInverso();
+         break;
+       
+       case 4:
+         encendidoAlterno();
+         break;
+       
+       case 5:
+         kitt();
+         break;
+       
+       default:
+         apagados();
+         break;
      
-      
       }
     
 }
 
-    void lecturaBoton(int pinBoton)
+    void lecturaBotons()
     {
-      int lectura=digitalRead(pinBoton);
-
-    if (lectura!=ultimoEstadoBotonMais) {
-      ultimoDebounce=millis();
-      }
-    
-    if ((millis()-ultimoDebounce)>retardoDebounce) 
-    {
-      if (lectura!=estadoBotonMais) 
+      for (int i=0;i<2;i++)
       {
-        estadoBotonMais=lectura;
-        
-        if (estadoBotonMais==HIGH) 
+      int lectura=digitalRead(pinBotons[i]);
+
+      if (lectura!=ultimoEstadoBoton[i]) 
         {
-          programa=programa+1;
-          Serial.println("programa");
-          Serial.println(programa);
+          ultimoDebounce=millis();
           }
-        }
+    
+      if ((millis()-ultimoDebounce)>retardoDebounce) 
+        {
+          if (lectura!=estadoBoton[i]) 
+          {
+            estadoBoton[i]=lectura;
+            
+            if (estadoBoton[i]==HIGH) 
+            {
+              if (i==0)
+              {
+              programa=programa+1;
+              if (programa>5)
+                  {
+                    programa=0;
+                  }
+              }
+              if(i==1)
+              {
+                programa=programa-1;
+                if (programa<0)
+                  {
+                    programa=5;
+                  }
+              }
+              
+              Serial.println("programa");
+              Serial.println(programa);
+              }
+            }
+          }
+       ultimoEstadoBoton[i]=lectura;
       }
     
-    ultimoEstadoBotonMais=lectura;
-    
     }
     
     
-    void updateShiftRegister()
-    {
-    digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, LSBFIRST, leds);
-    digitalWrite(latchPin, HIGH);
-    }
+  void updateShiftRegister()
+  {
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, leds);
+  digitalWrite(latchPin, HIGH);
+  }
     
   void apagados()
   {
@@ -138,16 +138,67 @@ void loop()
     updateShiftRegister();
   }
   
+  void acesos()
+  {
+    leds=255;
+    updateShiftRegister();
+  }
+  
   void encendidoProgresivo() 
   {
       leds = 0;
       updateShiftRegister();
-      delay(500);
+      delay(100);
       for (int i = 0; i < 8; i++)
       {
+      lecturaBotons();
       bitSet(leds, i);
       updateShiftRegister();
-      delay(500);
+      delay(100);
       }
     }
+    void encendidoProgresivoInverso() 
+  {
+      leds = 0;
+      updateShiftRegister();
+      delay(100);
+      for (int i = 0; i < 8; i++)
+      {
+      lecturaBotons();
+      bitSet(leds, 7-i);
+      updateShiftRegister();
+      delay(100);
+      }
+    }
+    
+     void encendidoAlterno() 
+  {
+      leds = B10101010;
+      updateShiftRegister();
+      delay(100);
+      leds=B01010101;
+      updateShiftRegister();
+      delay(100);
+      
+    }
+    void kitt() 
+  {
+      for (int i = 0; i < 8; i++)
+      {
+      lecturaBotons();
+      leds=0;
+      bitSet(leds, i);
+      updateShiftRegister();
+      delay(100);
+      }
+      for (int i = 1; i < 7; i++)
+      {
+      lecturaBotons();
+      leds=0;
+      bitSet(leds, 7-i);
+      updateShiftRegister();
+      delay(100);
+      }
+    }
+    
 //    
